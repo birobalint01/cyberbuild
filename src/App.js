5a03b7bdbb515981e2424e3ffb17d866e8e0b894
birobalint01 <112955179+birobalint01@users.noreply.github.com>
2023-02-21 11:48:06 +0100

    update2
    reg és login külön file-ba téve

--- src/App.js
+++ src/App.js
@@ -2,29 +2,21 @@ import './App.css';
 import Header from './Header'; 
 import React from 'react';
 import { BrowserRouter, Routes , Route, Link } from 'react-router-dom';
-import Register from './register';
+import Login from './Login';
+import Register from './Register';
 
 function App() {
   return (
     <BrowserRouter>
       <div style={{backgroundImage: `url(${require('./images/hatter.jpg')})`, height: '100vh' , backgroundRepeat: 'no-repeat' , backgroundSize: 'cover  '}}>
         <Header></Header>
-        <h1 className='asd' style={{marginBottom:'30px'}}>Login</h1>
-              <form className="asd">
-                  <label className='asd2' htmlFor="email">email</label>
-                  <input className='asd2' placeholder="youremail@gmail.com" id="email" name="email" style={{width:'250px' , height:'20px'}}/>
-                  <label className='asd2' htmlFor="password">password</label>
-                  <input className='asd2' type="password" placeholder="********" id="password" name="password" style={{width:'250px' , height:'20px'}}/>
-                  <button className='asd1' type="submit" style={{marginTop:'15px'}}>Log In</button>
-                  <Link to='/register' style={{marginTop:'30px'}}>Don't have an account? Register here.</Link>
-              </form>
-              <Routes>
-                <Route path='/register' component={<Register />} />
-              </Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
 }
 
 export default App;
- 
--- src/Login.js
+++ src/Login.js
@@ -3,23 +3,21 @@ import { Link } from 'react-router-dom';
 import Header from './Header'; 
 import './App.css';
 
-function Register() {
+function Login() {
   return (
     <div style={{backgroundImage: `url(${require('./images/hatter.jpg')})`, height: '100vh' , backgroundRepeat: 'no-repeat' , backgroundSize: 'cover  '}}>
       <Header></Header>
-      <h1 className='asd' style={{marginBottom:'30px'}}>Register</h1>
+      <h1 className='asd' style={{marginBottom:'30px'}}>Login</h1>
       <form className="asd">
         <label className='asd2' htmlFor="email">email</label>
         <input className='asd2' placeholder="youremail@gmail.com" id="email" name="email" style={{width:'250px' , height:'20px'}}/>
         <label className='asd2' htmlFor="password">password</label>
-        <input className='asd2' type="password" placeholder="Password" id="password" name="password" style={{width:'250px' , height:'20px'}}/>
-        <label className='asd2' htmlFor="password">repeat password</label>
-        <input className='asd2' type="password" placeholder="Repeat Password" id="password" name="password" style={{width:'250px' , height:'20px'}}/>
+        <input className='asd2' type="password" placeholder="********" id="password" name="password" style={{width:'250px' , height:'20px'}}/>
         <button className='asd1' type="submit" style={{marginTop:'15px'}}>Log In</button>
-        <Link to='/login' style={{marginTop:'30px'}}>Already have an Account? Why don't you log in?</Link>
+        <Link to='/register' style={{marginTop:'30px'}}>Don't have an account? Register here.</Link>
       </form>
     </div>
   );
 }
 
-export default Register;
+export default Login;
